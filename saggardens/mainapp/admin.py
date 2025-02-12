0eb178a092c9398038ccef09469fa45056cf1146
AlexTheMaggot <alexthemaggot23@gmail.com>
2025-02-12 16:02:56 +0500

    - add mainapp application

---
+++ saggardens/mainapp/admin.py
@@ -0,0 +1,41 @@
+from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import *
+
+
+class GardenAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title_en', ), }
+
+
+class GrowtimeAdmin(admin.ModelAdmin):
+    list_display = ('garden', 'year', 'nut', 'area', )
+
+
+class NutvarietyAdmin(admin.ModelAdmin):
+    list_display = ('garden', 'nut', 'area')
+
+
+class PostAdmin(SummernoteModelAdmin):
+    summernote_fields = ('text_ru', 'text_en', 'text_uzl', 'text_uzc')
+
+
+class_pairs = [
+    [Growtime, GrowtimeAdmin],
+    [Garden, GardenAdmin],
+    [Nutvariety, NutvarietyAdmin],
+    [Post, PostAdmin],
+]
+
+class_single = [
+    Nut,
+    Photo,
+    Author,
+    Category,
+    Product
+]
+
+for item in class_single:
+    admin.site.register(item)
+
+for item in class_pairs:
+    admin.site.register(item[0], item[1])
